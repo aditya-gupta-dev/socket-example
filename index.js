4f56996f420203ab3c3b42fe69c5c96dfbb6f2ed
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-05 15:38:46 +0530

    server with one endpoint

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import express from 'express';
+import http from 'http';
+import { utils } from './utils.js';
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    console.log(req.headers['user-agent'])
+    res.send('hello')
+});
+
+server.listen(utils.port, () => {
+    console.log(`Running at -> http://localhost:${utils.port}...`);
+});
+
+
 
