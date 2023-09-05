541f5fa7730dfe43bd37a85b2f84f12f595ff3a0
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-05 15:58:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
 import { utils } from './utils.js';
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    console.log(req.headers['user-agent'])
-    res.send('hello')
+    res.sendFile('H:/Javascript/socket/index.html')
+});
+
+io.on('connection', (socket) => { 
+    console.log(socket.client.request.headers['user-agent']);
 });
 
 server.listen(utils.port, () => {
     console.log(`Running at -> http://localhost:${utils.port}...`);
 });
-
 
 
